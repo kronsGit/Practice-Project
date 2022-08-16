1cd9211502aac56956e5eefe865997bb2133cb4a
SkollGit <front.devskoll@gmail.com>
2022-08-16 14:14:33 -0600

    Changed

--- JavaScriptPlatzi/canvasJs/curvas/curvas.js
+++ JavaScriptPlatzi/canvasJs/curvas/curvas.js
@@ -1,23 +1,23 @@
 let d = document.getElementById("curvas");
 let lienzo = d.getContext("2d");
-let lineas = 30;
+let lineas = 100;
 let l = 0;
 let yi,xf;
 
 while(l < lineas){
-    yi = 10 * l;
-    xf = 10 * (l+1);
-    dibujarLinea("green",0,yi,xf,300);
+    yi = 5 * l;
+    xf = 5 * (l+1);
+    dibujarLinea("green",0,yi,xf,500);
     console.log("Linea" +1);
 
     l=l+1;
 
 }
 
-dibujarLinea("green",0,0,10,300);
-dibujarLinea("green",0,0,20,300);
-dibujarLinea("green",0,0,30,300);
-dibujarLinea("green",0,0,40,300);
+// dibujarLinea("green",0,0,10,300);
+// dibujarLinea("green",0,0,20,300);
+// dibujarLinea("green",0,0,30,300);
+// dibujarLinea("green",0,0,40,300);
 
 
 // lienzo.beginPath();
