ba2d62f523b7db1ea82ddd127ca7801c4bebc5cf
SkollGit <front.devskoll@gmail.com>
2022-08-29 16:29:54 -0600

    Updates
    Code nre

---
+++ JavaScriptPlatzi/canvasJs/eventos/eventos.js
@@ -0,0 +1,44 @@
+let texto = document.getElementById("texto_lineas");
+let boton = document.getElementById("botoncito");
+boton.addEventListener();
+
+let d = document.getElementById("curvas");
+let lienzo = d.getContext("2d");
+let lineas = 1000;
+let l = 0;
+let yi,xf;
+
+while(l < lineas){
+    yi = 7 * l;
+    xf = 7 * (l+1);
+    dibujarLinea("green",0,yi,xf,1000);
+    console.log("Linea" +1);
+
+    l=l+1;
+
+}
+
+dibujarLinea("#000",1,1,1,999);
+dibujarLinea("#000",1,999,999,999);
+
+// dibujarLinea("green",0,0,10,300);
+// dibujarLinea("green",0,0,20,300);
+// dibujarLinea("green",0,0,30,300);
+// dibujarLinea("green",0,0,40,300);
+
+
+// lienzo.beginPath();
+// lienzo.strokeStyle = "red";
+// lienzo.moveTo(180,10);
+// lienzo.lineTo(290,200);
+// lienzo.stroke();
+// lienzo.closePath(); 
+
+function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial,yinicial);
+    lienzo.lineTo(xfinal,yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
