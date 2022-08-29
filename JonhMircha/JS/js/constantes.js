936eeee6f28e265c470f9eb47d07e148bed05cdb
SkollGit <front.devskoll@gmail.com>
2022-08-29 15:59:09 -0600

    Updates

---
+++ JonhMircha/JS/js/constantes.js
@@ -0,0 +1,15 @@
+export const PI = Math.PI;
+export let usuario = "Jon";
+
+
+const hello = ()=>console.log('Hola');
+
+export default function saludar(){
+    console.log("Hola Modulo +ES6");
+}
+
+export class Saludar{
+    constructor(){
+        console.log('Hola Clase +ES6');
+    }
+}
---
+++ JonhMircha/JS/js/modulos.js
@@ -0,0 +1,11 @@
+import saludar,{Saludar,PI}from "./constantes.js";
+import{aritmetica as Calcu}from "./aritmetica.js";
+
+
+console.log('Modulos.js');
+console.log(PI);
+console.log(Calcu.sumar(45,6));
+console.log(Calcu.restar(45,6));
+saludar();
+let saludo = new Saludar();
+saludo;
