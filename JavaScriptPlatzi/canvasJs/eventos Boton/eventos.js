1f9d9619b7c3b92e3f2d9e936fb689d988f1980a
SkollGit <front.devskoll@gmail.com>
2022-09-01 16:29:15 -0600

    New code

---
+++ JavaScriptPlatzi/canvasJs/eventos Boton/eventos.js
@@ -0,0 +1,6 @@
+//Accediendo al Todo de la pagina 
+document.addEventListener("keydown",dibujarTeclado); //Aqui le esta diciendo que cuando se toque esa tecla se ejecutara dicha funcion
+
+function dibujarTeclado(){
+    alert("Oh por dios");
+}
