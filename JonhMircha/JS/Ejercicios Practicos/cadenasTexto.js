cae9c7bb0071f99c137441afbd6554682d8fbb39
SkollGit <front.devskoll@gmail.com>
2022-09-06 16:33:40 -0600

    More Code

--- JonhMircha/JS/Ejercicios Practicos/cadenasTexto.js
+++ JonhMircha/JS/Ejercicios Practicos/cadenasTexto.js
@@ -89,11 +89,46 @@ const recortarTexto = (cadena = "",longitud=undefined)=>
 
 
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+const cadenaAArreglo = (cadena = "",separador = undefined)=>
+    (!cadena)   //Operador Terneario
+    ?console.warn("No ingresaste una cadena de texto")
+    :(separador === undefined)
+        ?console.warn("No ingresaste el caracter separador")
+        :console.info(cadena.split(separador));
+    
+        cadenaAArreglo();
+        cadenaAArreglo("Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum"," ");
+        cadenaAArreglo("Enero,Febrero"," ");
 
 
 
 
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. 
+const repetirTexto=(texto="",veces=undefined)=>{
+    if(!texto) return console.warn("No ingresaste un texto");
+
+    if(veces === undefined) return console.warn("No ingresaste el numero de veces a repetir");
+    
+    if(veces === 0) return console.error("El numero no de veces no puede ser 0");
+
+    if(Math.sign(veces)===-1) return console.error("El numero de veces no puede ser negativo");
+
+    for(let i=1;i<=veces;i++){
+        console.info(`${texto},${i}`);
+    }
+
+}
+
+
+repetirTexto("Hola mundo Programacion",10);
+repetirTexto("Hola mundo Programacion",0);
+repetirTexto("Hola mundo Programacion",-10);
+repetirTexto("",10);
+repetirTexto("Hola mundo Programacion");
+
+
+
+
 
 
 
