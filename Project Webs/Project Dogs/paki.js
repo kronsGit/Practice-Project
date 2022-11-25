2fcdd7df66586b3c792e89accad96292d88e8cab
kronsGit <109983643+kronsGit@users.noreply.github.com>
2022-11-25 14:47:00 -0600

    Updates

---
+++ Project Webs/Project Dogs/paki.js
@@ -0,0 +1,35 @@
+//Array asociativo
+let imagenes = [];
+imagenes["Hati"] = "img/Bobby.png";
+imagenes["Bobby"] = "img/Bobby.png";
+imagenes["Gurruminsota"] = "img/Bobby.png";
+imagenes["Gurrumincita"] = "img/Bobby.png";
+imagenes["Silvestre"] = "img/Bobby.png";
+imagenes["Panzer"] = "img/Bobby.png";
+imagenes["Pipiya"] = "img/Bobby.png";
+
+
+
+// Creando un array para guardar a todos los elementos
+let desk = [];
+
+desk.push(new Pakiman("Bobby",100,50,"Nivel normal"));
+desk.push(new Pakiman("Hati",100,150,"Nivel experto"));
+desk.push(new Pakiman("Gurruminsota",130,100,"Nivel Dios"));
+desk.push(new Pakiman("Gurrumincita",70,80,"Nivel medio"));
+desk.push(new Pakiman("Silvestre",90,20,"Nivel Dios"));
+desk.push(new Pakiman("Panzer",50,40,"Nivel Dios"));
+desk.push(new Pakiman("Pipiya",90,90,"Nivel Dios"));
+
+
+
+// Mostrar los elementos de array
+
+for(let allDesk of desk){
+    allDesk.show();
+}
+
+for(let x of desk){
+    console.log(x);
+}
+
---
+++ Project Webs/Project Dogs/pakiclass.js
@@ -0,0 +1,91 @@
+
+
+
+
+// Creando clase
+class Pakiman{
+    //Constructor
+    constructor(n,v,a,f){
+        this.image = new Image();
+        this.nombre=n;
+        this.vida=v;
+        this.ataque=a;
+        this.farmeo=f;
+
+        this.image.src = imagenes[this.nombre];
+    }
+    hablar(){
+        alert(this.nombre);
+    }
+
+    show(){
+
+       
+        //Div contenedor principal
+        let divmain = document.createElement('div');
+        divmain.setAttribute("class",`cardMain`);//Agregar una clase 
+
+        //H1 con titulo con el nombre del clan
+        let cabecera = document.createElement('h2');
+        cabecera.innerHTML = this.nombre;
+        cabecera.setAttribute("class","card");//Agregando una clase a "cabecera"
+        let saltoLinea = document.createElement('br');
+
+        let divchild = document.createElement('div');//Agregando pets-detail
+        divchild.setAttribute("class","childCard");//Agregando una clase a divChild
+
+        let spanStats = document.createElement('span');
+        spanStats.innerHTML = 'Stats';
+        
+        let pVida = document.createElement('p');//Creando el elemento p de la Vida
+        pVida.innerHTML = 'Vida: '+this.vida + '<br>';
+        let pAtaque = document.createElement('p');//Creando el elemento p del ataque
+        pAtaque.innerHTML ='Ataque: '+this.ataque + '<br>';
+        let pFarmeo = document.createElement('p');//Creando el elemento p del farmeo
+        pFarmeo.innerHTML ='Farmeo: '+this.farmeo + '<br>';
+        
+        //Agregando los p al divChild
+        divchild.appendChild(spanStats);
+        divchild.appendChild(pVida);
+        divchild.appendChild(pAtaque);
+        divchild.appendChild(pFarmeo);
+        
+ 
+        
+        //Creando la card
+        divmain.appendChild(this.image);
+        divmain.appendChild(saltoLinea);
+        divmain.appendChild(cabecera);
+        divmain.appendChild(divchild);
+        //Agregando al Body
+       
+        document.body.appendChild(divmain);
+        
+        
+
+
+
+        // document.body.appendChild(this.image);
+        // document.write("<div class=card>")
+        // document.write('<br/>');
+        // document.write('<br/> <strong>' + this.nombre + '</strong>');
+        // document.write('<br/>');
+        // document.write('Vida: ' + this.vida + '<br/>');
+        // document.write('<br/>');
+        // document.write('Ataque: ' + this.ataque + '<br/>');
+        // document.write('<br/>');
+        // document.write('Farmeo: ' + this.farmeo + '<br/>');
+        // document.write("</div>");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
