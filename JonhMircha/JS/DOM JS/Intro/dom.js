0213b47116655c2febc6e1673a2c6267114fd084
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-01-11 12:03:52 -0600

    add code

---
+++ JonhMircha/JS/DOM JS/Intro/dom.js
@@ -0,0 +1,13 @@
+console.log("******Elementos del Dom********");
+console.log(window.document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement); //Html etiqueta
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+document.write("<h2>Hola mundo desde el DOM</h2>")
