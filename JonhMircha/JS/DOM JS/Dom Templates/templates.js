18154d243702bfae90a80a586de6313134be2855
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-02-20 09:03:47 -0600

    add

--- JonhMircha/JS/DOM JS/Dom Templates/templates.js
+++ JonhMircha/JS/DOM JS/Dom Templates/templates.js
@@ -2,6 +2,7 @@
 const $cards = document.querySelector(".cards"), //Seleecionando el elemento en donde se dibujara el template dinamicamente
       $template = document.getElementById("template-card").content, //Seleccionando elementos del templates
       $fragment = document.createDocumentFragment(),
+      //Arreglo donde esta la informacion de las imagenes
       cardContent = [
             {
                 title: "Tecnologia",
@@ -25,16 +26,18 @@ const $cards = document.querySelector(".cards"), //Seleecionando el elemento en 
             },
         ];
 
+       //Foreach para recorrer los elementos del arreglo 
       cardContent.forEach(el=>{
-        $template.querySelector("img").setAttribute("src",el.img);
-        $template.querySelector("img").setAttribute("alt",el.title);
-        $template.querySelector("figcaption").textContent = el.title;
 
-        let $clone = document.importNode($template,true);
-        $fragment.appendChild($clone);
+        $template.querySelector("img").setAttribute("src",el.img); //Recorriendo los elementos del arreglo en este caso el atributo "src" de las imagenes
+        $template.querySelector("img").setAttribute("alt",el.title);//Recorriendo los elementos del arreglo, en este caso el atributo "alt"
+        $template.querySelector("figcaption").textContent = el.title; //Recorriendo los elementos del arreglo
+
+        let $clone = document.importNode($template,true); //Clonando el template
+        $fragment.appendChild($clone); //Agregando el clone al fragmento
       });
 
-      $cards.appendChild($fragment);
+      $cards.appendChild($fragment); //Agregando el fragmento al padre cards
 
 
 
