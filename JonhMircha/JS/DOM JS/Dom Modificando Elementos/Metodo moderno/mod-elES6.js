9295c5652e5e5ad2cf9aa7e62e50ff966806ae91
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-02-22 11:05:40 -0600

    add

--- JonhMircha/JS/DOM JS/Dom Modificando Elementos/Metodo moderno/mod-elES6.js
+++ JonhMircha/JS/DOM JS/Dom Modificando Elementos/Metodo moderno/mod-elES6.js
@@ -1,21 +1,45 @@
 
-//Forma antigua
+//Forma Moderna
+
+/*
+.insertAdjacent...
+  .insertAdjacentElement(position,el);
+  .insertAdjacentHTML(position,html);
+  .insertAdjacentText(position,text);
+Posiciones:
+  beforebegin(hermano anterior)
+  afterbegin(primer hijo)
+  beforeend(ultimo hijo)
+  afterend(hermano siguiente)
+*/
 
 const $cards = document.querySelector(".cards"), //Seleccionando el elemento con la clase card
-      $newCard = document.createElement("figure"), //Creando un elemento figure
-      $cloneCards = $cards.cloneNode(true); //Clonando los elementos creados
-
-      //Agregandole elementos a la nueva variable newCars
-      $newCard.innerHTML = `
+      $newCard = document.createElement("figure"); //Creando un elemento figure
+     
+      //Nueva forma de agregar un elemento
+      let $contentCard = `
         <img src="https://placeimg.com/350/350/any" alt="Any">
-        <figcaption>Any</figcaption>
+        <figcaption></figcaption>
       `;
       $newCard.classList.add("card"); //Agregandole una clase a el elemento newCard
 
-    //   $cards.replaceChild($newCard,$cards.children[2]); //Replaza un elemento, primero se selecciona el padre y se usa el metodo, en los parametros del metodo se pone el nuevo elemento por el cual sera remplazado.
-    //   $cards.removeChild($cards.lastElementChild); //Aqui este removiendo el ultimo elemento del padre $cards
-      $cards.insertBefore($newCard,$cards.firstElementChild); //Aqui se esta agregando el newcard antes del primer elemento
-      document.body.appendChild($cloneCards);//Se esta agregando el body el nuevo elemento
+     
+      // $cards.insertAdjacentElement("afterbegin",$newCard); //Agregando la nueva Card
+      //Otra forma de agregar elementos
+      // $cards.prepend($newCard); //Primer hijo de $cards
+      $cards.append($newCard);
+      $newCard.insertAdjacentHTML("beforeend",$contentCard); //Agregando el contenido de la nueva card que en este caso es todo lo que contiene la variable $content Card
+      $newCard.querySelector("figcaption").insertAdjacentText("afterbegin","Nuevo Texto para Card");
+
+      // $cards.insertAdjacentElement("beforebegin",$newCard); //Inserta un elemento antes de la referencia que este caso es $cards
+      // $cards.insertAdjacentElement("afterbegin",$newCard); //Inserta un elemento como primer hijo de la referencia que este caso es $cards
+      //  $cards.insertAdjacentElement("beforeend",$newCard);    //Inserta un elemento como ultimo hijo de la referencia que este caso es $cards
+      //  $cards.insertAdjacentElement("afterend",$newCard); //Inserta un elemento despues de la referencia que este caso es $cards
+
+      
+
+
+
       
 
 
