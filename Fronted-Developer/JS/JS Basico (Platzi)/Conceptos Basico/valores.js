e6634ce003f847c6bf5190347c82ac0e433b46a6
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-07-12 17:29:17 -0600

    Add code

---
+++ Fronted-Developer/JS/JS Basico (Platzi)/Conceptos Basico/valores.js
@@ -0,0 +1,14 @@
+// Valores Primitivos
+
+/*Numero*/  47
+/*Cadena de Texto*/ "Ivan Juarez"
+/*Booleano*/ true
+/*Booleano*/ false
+null
+undefined
+
+
+// Valores  compuestos
+
+/*Objeto*/ [1,2,3];
+/*Objeto*/ {nombre:"Diego"};
---
+++ Fronted-Developer/JS/JS Basico (Platzi)/Readme.md
@@ -0,0 +1,2 @@
+# Curso basico JavaScript
+Primeros conceptos de JavaScript
