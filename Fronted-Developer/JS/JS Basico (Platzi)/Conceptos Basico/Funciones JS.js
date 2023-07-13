33027d90001d9828d3db13186d35542bda6ef9be
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-07-12 18:51:40 -0600

    code

---
+++ Fronted-Developer/JS/JS Basico (Platzi)/Conceptos Basico/Funciones JS.js
@@ -0,0 +1,13 @@
+// Declarativas
+function miFuncion(){
+    return 3;
+}
+
+// Expresion o Anonimas 
+let funcion = function(a,b){
+    return console.log(a+b);
+}
+
+funcion(3,6);
+funcion(20,2);
+funcion(10,20);
