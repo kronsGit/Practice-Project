6579b40f62f4f26fc62db9d6fe1f5b572b0e1232
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-08-27 21:10:36 -0600

    Code complete

--- Fronted-Developer/JS/JS Basico (Platzi)/Conceptos Basico/contructor.js
+++ Fronted-Developer/JS/JS Basico (Platzi)/Conceptos Basico/contructor.js
@@ -1,5 +1,7 @@
 //Lista de autos vacia
 let listaAutos = [];
+let i = 0;
+let conte = 0;
 
 //Funccion constructura
 function auto (marca,modelo,annio){
@@ -7,22 +9,23 @@ function auto (marca,modelo,annio){
     this.modelo = modelo;
     this.annio = annio;
 }
-//Funcion para agregar autos
-let agregarAuto = (marca,modelo,annio) =>{
-    let nuevoAuto = new auto(marca,modelo,annio);
-    listaAutos.push(nuevoAuto);
-}
+
 
 //Funcion del nuevo auto con sus parametros
-let registroAuto = () =>{
-    let marca = prompt(`Ingrese la marca del carro`);
-    let modelo = prompt(`Ingrese el modelo del carro`);
-    let annio = prompt(`Ingrese el annio de fabricacion del carro`);
-    agregarAuto(marca,modelo,annio);
+let registroAuto = (marca,modelo,annio) =>{
+    marca = prompt(`Ingrese la marca del carro`);
+    modelo = prompt(`Ingrese el modelo del carro`);
+    annio = prompt(`Ingrese el annio de fabricacion del carro`);
+
+    let nuevoAuto = new auto(marca,modelo,annio);
+    listaAutos.push(nuevoAuto);
     console.log("Agregaste un nuevo carro con exito");
+    conte++;
 }
 
 while(listaAutos.length < 2){
     registroAuto();
+    console.log(listaAutos[i]);
+    i++;
 }
 
