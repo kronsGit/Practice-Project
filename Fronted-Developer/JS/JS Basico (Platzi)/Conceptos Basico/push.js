246cb3583b6af3d5f176f399844847964764d8c0
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-09-01 22:29:43 -0600

    code

---
+++ Fronted-Developer/JS/JS Basico (Platzi)/Conceptos Basico/push.js
@@ -0,0 +1,9 @@
+//El metodo push() nos permite agregar un o mas elemenntos al final de un array.from(selector, {duration: 1, fromVars})
+let numArray = [1,2,3,4,5];
+
+//funcion
+let newNum = ()=>{
+    //Agregando elementos
+    numArray.push(6,7);
+    //Verificacion
+}
