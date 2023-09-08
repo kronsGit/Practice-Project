2d7b680af2a67a46fccab106188ac535d35d5aa4
kronsGit <109983643+kronsGit@users.noreply.github.com>
2023-09-07 19:59:51 -0600

    code

---
+++ Fronted-Developer/JS/JS Practico/Funciones/funciones.js
@@ -0,0 +1,26 @@
+//Funcion de nombre completo
+
+let nombre ="Ivan";
+let apellido = "Juarez";
+
+let nombreCompleto = (name,lastName) =>{
+   return `${name} ${lastName}`;
+}
+
+let saludo = (name,lastName,username) =>{
+    const completeName = nombreCompleto(name,lastName);
+
+    console.log(`Mi nombre es ${completeName}, pero prefiero que me digas ${username}`);
+}
+
+
+//Usando argumentos normales
+nombreCompleto('Sofia',"Perez");
+nombreCompleto("Juan","Guerrero");
+
+//Usando argumentos de variables ya creadas
+nombreCompleto(nombre,apellido);
+
+
+//Saludo
+saludo("Ivan","Juarez","Kronstharor");
