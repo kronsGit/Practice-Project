612e847f38d292b2fab46e2264138487cbaa6c48
kronsGit <109983643+kronsGit@users.noreply.github.com>
2024-01-08 15:36:50 -0600

    updates

--- Fronted-Developer/JS/JS Practico/DOM JS/main.js
+++ Fronted-Developer/JS/JS Practico/DOM JS/main.js
@@ -1,1 +1,15 @@
-console.log("Helooo 2024")
+//Seleccionando elementor de Html
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
